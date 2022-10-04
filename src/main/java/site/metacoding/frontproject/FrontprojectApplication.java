e5b41a54c1ea33751bde4c8f776813eab77421cc
jinsa <jinsa004@gmail.com>
2022-10-04 13:48:48 +0900

    프로젝트 프론트 기초세팅

---
+++ src/main/java/site/metacoding/frontproject/FrontprojectApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.frontproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontprojectApplication.class, args);
+	}
+
+}
