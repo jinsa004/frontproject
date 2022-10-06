259543f1c5258f081e434b60b682b0319b1f7045
jinsa <jinsa004@gmail.com>
2022-10-06 11:05:18 +0900

    DB전체테이블 Entity, Dao 기초완료

---
+++ src/main/java/site/metacoding/frontproject/domain/subscribe/Subscribe.java
@@ -0,0 +1,12 @@
+package site.metacoding.frontproject.domain.subscribe;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Subscribe {
+    private Integer subscribeId;
+    private Integer employeeId;
+    private Integer companyId;
+}
