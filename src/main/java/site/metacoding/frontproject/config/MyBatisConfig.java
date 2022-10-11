a720fd5a0bd246ace4824371cc8c41307d054696
jinsa <jinsa004@gmail.com>
2022-10-11 11:32:04 +0900

    [이나윤]merge 충돌오류 해결완료

--- src/main/java/site/metacoding/frontproject/config/MyBatisConfig.java
+++ src/main/java/site/metacoding/frontproject/config/MyBatisConfig.java
@@ -11,16 +11,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
-@MapperScan(basePackages = "site.metacoding.baseball.domain")
+@MapperScan(basePackages = "site.metacoding.frontproject.domain")
 public class MyBatisConfig {
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-
+        
         sessionFactory.setDataSource(dataSource);
         sessionFactory.setMapperLocations(
-        		new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+              new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        //Resource myBatisConfig = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml");
+        //sessionFactory.setConfigLocation(myBatisConfig);
+        
+        org.apache.ibatis.session.Configuration config = 
+              new org.apache.ibatis.session.Configuration();
+        config.setMapUnderscoreToCamelCase(true);
+        sessionFactory.setConfiguration(config);
         return sessionFactory.getObject();
     }
 
